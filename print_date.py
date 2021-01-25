b969c56bbfb4d5834f8400f705104abce5c675f9
B03r <65600585+B03r@users.noreply.github.com>
2021-01-25 17:19:31 +0100

    New feature

--- print_date.py
+++ print_date.py
@@ -4,3 +4,6 @@ now = datetime.datetime.now()
 print ("Current date: ")
 print (now.strftime("%Y-%m-%d"))
 
+# Feature 1 - print date and time
+print ("Current date and time: ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
